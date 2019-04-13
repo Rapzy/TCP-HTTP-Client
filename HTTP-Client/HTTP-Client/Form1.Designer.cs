b3924c43068ff616a959779aa48297da7a3c9509
= <andrushko.egor@gmail.com>
2019-04-13 20:44:05 +0300

    Add solved task

---
+++ HTTP-Client/HTTP-Client/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace HTTP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.methodsComBox = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(477, 380);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(261, 31);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 21);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // methodsComBox
+            // 
+            this.methodsComBox.FormattingEnabled = true;
+            this.methodsComBox.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "HEAD"});
+            this.methodsComBox.Location = new System.Drawing.Point(12, 31);
+            this.methodsComBox.Name = "methodsComBox";
+            this.methodsComBox.Size = new System.Drawing.Size(75, 21);
+            this.methodsComBox.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Method";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.methodsComBox);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox methodsComBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HTTP-Client/HTTP-Client/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace HTTP_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            methodsComBox.SelectedIndex = 0;
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            string url = textBox2.Text;
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(url, 80);
+            NetworkStream stream = tcpClient.GetStream();
+            string request =
+                $"{methodsComBox.SelectedItem} / HTTP/1.0\r\n" +
+                $"Host: {url}\r\n" +
+                "\r\n";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(request);
+            stream.Write(data, 0, data.Length);
+            textBox1.Text += request;
+            data = new byte[1024];
+            int bytes;
+            string response = "";
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                response += Encoding.UTF8.GetString(data, 0, bytes);
+            } while (stream.DataAvailable);
+            tcpClient.Close();
+            textBox1.Text += response;
+
+        }
+    }
+}
